7ed1db3baa32711964bde36892ef6e0d20ea3264
alfsykr <alfis7433@gmail.com>
2026-02-27 14:56:09 +0700

    feat : fixed error for raport personal program

--- resources/views/admin/personal_program/raport.blade.php
+++ resources/views/admin/personal_program/raport.blade.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8">
   <title>{{$title}}</title>
   <style>
-    @page { margin: 25mm; }
+    @page {
+      margin: 25mm;
+    }
+
     body {
       font-family: "Times New Roman", serif;
       font-size: 12pt;
@@ -17,7 +21,7 @@
     }
 
     /* Watermark sebagai layer belakang */
- 
+
 
     .header-title {
       font-size: 18pt;
@@ -26,14 +30,20 @@
       margin-bottom: 10px;
     }
 
-    table { width: 100%; border-collapse: collapse; }
+    table {
+      width: 100%;
+      border-collapse: collapse;
+    }
 
-    .info td { padding: 2px 0; }
+    .info td {
+      padding: 2px 0;
+    }
 
     .box {
       border: 1.5px solid #000;
       margin-top: 10px;
     }
+
     .box th {
       border: 1.5px solid #000;
       padding: 6px;
@@ -41,6 +51,7 @@
       text-align: center;
       background: #fff;
     }
+
     .box td {
       border: 1px solid #000;
       padding: 8px;
@@ -58,70 +69,76 @@
 <body>
 
 
-<div class="header-title">Laporan Program Personal</div>
+  <div class="header-title">Laporan Program Personal</div>
 
-<table class="info">
-  <tr>
-    <td style="width: 18%;">Nama</td><td style="width:2%;">:</td>
-    <td style="width:30%;">{{ ucwords(strtolower($siswa->nama_lengkap)) }}</td>
-    <td style="width:18%;">Semester</td><td style="width:2%;">:</td>
-    <td style="width:30%;">{{$semester}}</td>
-  </tr>
-  <tr>
-    <td>Kelas/Program</td><td>:</td>
-    <td>{{$siswa->kelas->nama_kelas ?? '-'}}</td>
-    <td>Tahun Ajaran</td><td>:</td>
-    <td>{{$siswa->kelas->tapel->tahun_pelajaran ?? '-'}}</td>
-  </tr>
-</table>
+  <table class="info">
+    <tr>
+      <td style="width: 18%;">Nama</td>
+      <td style="width:2%;">:</td>
+      <td style="width:30%;">{{ ucwords(strtolower($siswa->nama_lengkap)) }}</td>
+      <td style="width:18%;">Semester</td>
+      <td style="width:2%;">:</td>
+      <td style="width:30%;">{{$semester}}</td>
+    </tr>
+    <tr>
+      <td>Kelas/Program</td>
+      <td>:</td>
+      <td>{{$siswa->kelas->nama_kelas ?? '-'}}</td>
+      <td>Tahun Ajaran</td>
+      <td>:</td>
+      <td>{{$siswa->kelas->tapel->tahun_pelajaran ?? '-'}}</td>
+    </tr>
+  </table>
 
-<table class="box">
-  <tr>
-    <th style="width:50%;">Laporan Program Personal {{ ucwords(strtolower($siswa->nama_lengkap)) }}</th>
-    <th style="width:50%;">Evaluasi Perkembangan Personal {{ ucwords(strtolower($siswa->nama_lengkap)) }}</th>
-  </tr>
-  <tr>
-    <td>
-      <ol style="margin:0; padding-left:18px;">
-        <li><strong>Motorik Kasar</strong>, {{$pp->motorik_kasar ?? '-'}}</li>
-        <li><strong>Sosialisasi</strong>, {{$pp->sosialisasi ?? '-'}}</li>
-        <li><strong>Rentang Akademis</strong>, {{$pp->rentang_akademis ?? '-'}}</li>
-      </ol>
-    </td>
-    <td>
-      <ol style="margin:0; padding-left:18px;">
-        <li><strong>Sosialisasi</strong>, {{$pp->evaluasi_sosialisasi ?? '-'}}</li>
-        <li><strong>Rentang Akademis</strong>, {{$pp->evaluasi_rentang_akademis ?? '-'}}</li>
-      </ol>
-    </td>
-  </tr>
-</table>
+  <table class="box">
+    <tr>
+      <th style="width:50%;">Laporan Program Personal {{ ucwords(strtolower($siswa->nama_lengkap)) }}</th>
+      <th style="width:50%;">Evaluasi Perkembangan Personal {{ ucwords(strtolower($siswa->nama_lengkap)) }}</th>
+    </tr>
+    <tr>
+      <td>
+        <ol style="margin:0; padding-left:18px;">
+          <li><strong>Motorik Kasar</strong>, {{ optional($pp)->motorik_kasar ?? '-' }}</li>
+          <li><strong>Sosialisasi</strong>, {{ optional($pp)->sosialisasi ?? '-' }}</li>
+          <li><strong>Rentang Akademis</strong>, {{ optional($pp)->rentang_akademis ?? '-' }}</li>
+        </ol>
+      </td>
+      <td>
+        <ol style="margin:0; padding-left:18px;">
+          <li><strong>Sosialisasi</strong>, {{ optional($pp)->evaluasi_sosialisasi ?? '-' }}</li>
+          <li><strong>Rentang Akademis</strong>, {{ optional($pp)->evaluasi_rentang_akademis ?? '-' }}</li>
+        </ol>
+      </td>
+    </tr>
+  </table>
 
-<table class="signature">
-  <tr>
-    <td style="text-align:left; width:50%;"></td>
-    <td style="text-align:right; width:50%;">
-      {{$siswa->kelas->tapel->kota ?? ''}}, {{ \Carbon\Carbon::now()->translatedFormat('d F Y') }}
-    </td>
-  </tr>
+  <table class="signature">
+    <tr>
+      <td style="text-align:left; width:50%;"></td>
+      <td style="text-align:right; width:50%;">
+        {{$siswa->kelas->tapel->kota ?? ''}}, {{ \Carbon\Carbon::now()->translatedFormat('d F Y') }}
+      </td>
+    </tr>
 
-  <tr>
-    <td style="padding-top:50px; text-align:left;">
-      Guru Kelas<br><br><strong>({{ $pp->guru->nama_lengkap ? ucwords(strtolower($pp->guru->nama_lengkap)) : '-' }})</strong>
-    </td>
+    <tr>
+      <td style="padding-top:50px; text-align:left;">
+        Guru
+        Kelas<br><br><strong>({{ optional(optional($pp)->guru)->nama_lengkap ? ucwords(strtolower(optional(optional($pp)->guru)->nama_lengkap)) : '-' }})</strong>
+      </td>
 
-    <td style="padding-top:50px; text-align:right;">
-      Orang Tua / Wali<br><br><strong>(....................)</strong>
-    </td>
-  </tr>
+      <td style="padding-top:50px; text-align:right;">
+        Orang Tua / Wali<br><br><strong>(....................)</strong>
+      </td>
+    </tr>
 
-  <tr>
-    <td colspan="2" style="text-align:center; padding-top:40px;">
-      Mengetahui,<br>Kepala Sekolah<br><br><br>
-      <strong>( Ni Made Desi Handayani, S.Pd.I, MM. )</strong>
-    </td>
-  </tr>
-</table>
+    <tr>
+      <td colspan="2" style="text-align:center; padding-top:40px;">
+        Mengetahui,<br>Kepala Sekolah<br><br><br>
+        <strong>( Ni Made Desi Handayani, S.Pd.I, MM. )</strong>
+      </td>
+    </tr>
+  </table>
 
 </body>
+
 </html>
