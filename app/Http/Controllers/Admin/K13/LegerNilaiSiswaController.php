add0b9870182648e2744bd30413586b1a902bb04
alfsykr <alfis7433@gmail.com>
2026-02-27 14:52:09 +0700

    feat : fixed view for leger nilai siswa

--- app/Http/Controllers/Admin/K13/LegerNilaiSiswaController.php
+++ app/Http/Controllers/Admin/K13/LegerNilaiSiswaController.php
@@ -2,29 +2,21 @@
 
 namespace App\Http\Controllers\Admin\K13;
 
-use App\AnggotaEkstrakulikuler;
 use App\AnggotaKelas;
-use App\Ekstrakulikuler;
-use App\Exports\AdminK13LegerNilaiExport;
 use App\Http\Controllers\Controller;
-use App\K13DeskripsiSikapSiswa;
-use App\K13MappingMapel;
-use App\K13NilaiAkhirRaport;
+use App\K13NilaiKisi;
+use App\K13RencanaKisi;
+use App\KehadiranSiswa;
 use App\Kelas;
-use App\Mapel;
-use App\NilaiEkstrakulikuler;
 use App\Pembelajaran;
+use App\Exports\AdminK13LegerNilaiExport;
+use App\Sekolah;
 use App\Tapel;
 use Illuminate\Http\Request;
 use Excel;
 
 class LegerNilaiSiswaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $title = 'Leger Nilai Siswa';
@@ -32,83 +24,57 @@ class LegerNilaiSiswaController extends Controller
         return view('admin.k13.leger.pilihkelas', compact('title', 'data_kelas'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $title = 'Leger Nilai Siswa';
         $tapel = Tapel::findorfail(session()->get('tapel_id'));
         $kelas = Kelas::findorfail($request->kelas_id);
-        $data_kelas = Kelas::where('tapel_id', session()->get('tapel_id'))->get();
-
-        $data_id_mapel_semester_ini = Mapel::where('tapel_id', $tapel->id)->get('id');
-
-        $data_id_mapel_kelompok_a = K13MappingMapel::whereIn('mapel_id', $data_id_mapel_semester_ini)->where('kelompok', 'A')->get('mapel_id');
-        $data_id_mapel_kelompok_b = K13MappingMapel::whereIn('mapel_id', $data_id_mapel_semester_ini)->where('kelompok', 'B')->get('mapel_id');
-
-        $data_id_pembelajaran_all = Pembelajaran::where('kelas_id', $kelas->id)->get('id');
-        $data_id_pembelajaran_a = Pembelajaran::where('kelas_id', $kelas->id)->whereIn('mapel_id', $data_id_mapel_kelompok_a)->get('id');
-        $data_id_pembelajaran_b = Pembelajaran::where('kelas_id', $kelas->id)->whereIn('mapel_id', $data_id_mapel_kelompok_b)->get('id');
-
-        $data_mapel_kelompok_a = K13NilaiAkhirRaport::whereIn('pembelajaran_id', $data_id_pembelajaran_a)->groupBy('pembelajaran_id')->get();
-        $data_mapel_kelompok_b = K13NilaiAkhirRaport::whereIn('pembelajaran_id', $data_id_pembelajaran_b)->groupBy('pembelajaran_id')->get();
-
-        $data_ekstrakulikuler = Ekstrakulikuler::where('tapel_id', $tapel->id)->get();
-        $count_ekstrakulikuler = count($data_ekstrakulikuler);
+        $data_kelas = Kelas::where('tapel_id', $tapel->id)->get();
+        $sekolah = Sekolah::first();
 
         $data_anggota_kelas = AnggotaKelas::where('kelas_id', $kelas->id)->get();
+
         foreach ($data_anggota_kelas as $anggota_kelas) {
+            // Ambil semua pembelajaran di kelas ini
+            $pembelajaran_list = Pembelajaran::with(['mapel.k13_mapping_mapel'])
+                ->where('kelas_id', $kelas->id)
+                ->get()
+                ->sortBy(function ($item) {
+                    return optional($item->mapel->k13_mapping_mapel)->nomor_urut ?? 99;
+                });
 
-            $data_nilai_kelompok_a = K13NilaiAkhirRaport::whereIn('pembelajaran_id', $data_id_pembelajaran_a)->where('anggota_kelas_id', $anggota_kelas->id)->get();
-            $data_nilai_kelompok_b = K13NilaiAkhirRaport::whereIn('pembelajaran_id', $data_id_pembelajaran_b)->where('anggota_kelas_id', $anggota_kelas->id)->get();
+            $total_nilai = 0;
+            $count_mapel = 0;
 
-            $anggota_kelas->data_nilai_kelompok_a = $data_nilai_kelompok_a;
-            $anggota_kelas->data_nilai_kelompok_b = $data_nilai_kelompok_b;
+            foreach ($pembelajaran_list as $pembelajaran) {
+                $rencana_ids = K13RencanaKisi::where('pembelajaran_id', $pembelajaran->id)->pluck('id');
+                if ($rencana_ids->isEmpty())
+                    continue;
 
-            $rt_pengetahuan = K13NilaiAkhirRaport::whereIn('pembelajaran_id', $data_id_pembelajaran_all)->where('anggota_kelas_id', $anggota_kelas->id)->avg('nilai_pengetahuan');
-            $rt_keterampilan = K13NilaiAkhirRaport::whereIn('pembelajaran_id', $data_id_pembelajaran_all)->where('anggota_kelas_id', $anggota_kelas->id)->avg('nilai_keterampilan');
+                $avg = K13NilaiKisi::whereIn('k13_rencana_kisi_id', $rencana_ids)
+                    ->where('anggota_kelas_id', $anggota_kelas->id)
+                    ->avg('nilai');
 
-            $anggota_kelas->rata_rata_pengetahuan = round($rt_pengetahuan, 0);
-            $anggota_kelas->rata_rata_keterampilan = round($rt_keterampilan, 0);
+                if (is_null($avg))
+                    continue;
 
-            $cek_deskripsi_sikap = K13DeskripsiSikapSiswa::where('anggota_kelas_id', $anggota_kelas->id)->first();
-            if (is_null($cek_deskripsi_sikap)) {
-                $anggota_kelas->nilai_spiritual = '-';
-                $anggota_kelas->nilai_sosial = '-';
-            } else {
-                $anggota_kelas->nilai_spiritual = $cek_deskripsi_sikap->nilai_spiritual;
-                $anggota_kelas->nilai_sosial = $cek_deskripsi_sikap->nilai_sosial;
+                $total_nilai += round($avg, 0);
+                $count_mapel++;
             }
 
-            $anggota_kelas->data_nilai_ekstrakulikuler = Ekstrakulikuler::where('tapel_id', $tapel->id)->get();
-
-            foreach ($anggota_kelas->data_nilai_ekstrakulikuler as $data_nilai_ekstrakulikuler) {
-                $cek_anggota_ekstra = AnggotaEkstrakulikuler::where('ekstrakulikuler_id', $data_nilai_ekstrakulikuler->id)->where('anggota_kelas_id', $anggota_kelas->id)->first();
-                if (is_null($cek_anggota_ekstra)) {
-                    $data_nilai_ekstrakulikuler->nilai = '-';
-                } else {
-                    $cek_nilai_ekstra = NilaiEkstrakulikuler::where('ekstrakulikuler_id', $data_nilai_ekstrakulikuler->id)->where('anggota_ekstrakulikuler_id', $cek_anggota_ekstra->id)->first();
-                    if (is_null($cek_nilai_ekstra)) {
-                        $data_nilai_ekstrakulikuler->nilai = '-';
-                    } else {
-                        $data_nilai_ekstrakulikuler->nilai = $cek_nilai_ekstra->nilai;
-                    }
-                }
-            }
+            $anggota_kelas->rata_rata_semua = $count_mapel > 0 ? round($total_nilai / $count_mapel, 0) : '-';
+            $anggota_kelas->kehadiran_siswa = KehadiranSiswa::where('anggota_kelas_id', $anggota_kelas->id)->first();
         }
-        return view('admin.k13.leger.index', compact('title', 'kelas', 'data_kelas', 'data_mapel_kelompok_a', 'data_mapel_kelompok_b', 'data_ekstrakulikuler', 'count_ekstrakulikuler', 'data_anggota_kelas'));
+
+        return view('admin.k13.leger.index', compact(
+            'title',
+            'kelas',
+            'data_kelas',
+            'sekolah',
+            'data_anggota_kelas'
+        ));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         $kelas = Kelas::findorfail($id);
--- app/Http/Controllers/WaliKelas/K13/LihatLegerNilaiController.php
+++ app/Http/Controllers/WaliKelas/K13/LihatLegerNilaiController.php
@@ -2,19 +2,16 @@
 
 namespace App\Http\Controllers\WaliKelas\K13;
 
-use App\AnggotaEkstrakulikuler;
 use App\AnggotaKelas;
-use App\Ekstrakulikuler;
 use App\Exports\WaliKelasLegerNilaiExport;
 use App\Guru;
 use App\Http\Controllers\Controller;
-use App\K13DeskripsiSikapSiswa;
-use App\K13MappingMapel;
-use App\K13NilaiAkhirRaport;
+use App\K13NilaiKisi;
+use App\K13RencanaKisi;
+use App\KehadiranSiswa;
 use App\Kelas;
-use App\Mapel;
-use App\NilaiEkstrakulikuler;
 use App\Pembelajaran;
+use App\Sekolah;
 use App\Tapel;
 use Illuminate\Http\Request;
 use Excel;
@@ -22,71 +19,48 @@ use Illuminate\Support\Facades\Auth;
 
 class LihatLegerNilaiController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $title = 'Leger Nilai Siswa';
         $tapel = Tapel::findorfail(session()->get('tapel_id'));
         $guru = Guru::where('user_id', Auth::user()->id)->first();
         $id_kelas_diampu = Kelas::where('tapel_id', $tapel->id)->where('guru_id', $guru->id)->get('id');
-
-        $data_id_mapel_semester_ini = Mapel::where('tapel_id', $tapel->id)->get('id');
-
-        $data_id_mapel_kelompok_a = K13MappingMapel::whereIn('mapel_id', $data_id_mapel_semester_ini)->get('mapel_id');
-
-        $data_id_pembelajaran_all = Pembelajaran::whereIn('kelas_id', $id_kelas_diampu)->get('id');
-        $data_id_pembelajaran_a = Pembelajaran::whereIn('kelas_id', $id_kelas_diampu)->whereIn('mapel_id', $data_id_mapel_kelompok_a)->get('id');
-
-        $data_mapel_kelompok_a = K13NilaiAkhirRaport::whereIn('pembelajaran_id', $data_id_pembelajaran_a)->groupBy('pembelajaran_id')->get();
-        $data_mapel_kelompok_b = collect(); // Kelompok B tidak digunakan lagi
-
-        $data_ekstrakulikuler = Ekstrakulikuler::where('tapel_id', $tapel->id)->get();
-        $count_ekstrakulikuler = count($data_ekstrakulikuler);
+        $sekolah = Sekolah::first();
 
         $data_anggota_kelas = AnggotaKelas::whereIn('kelas_id', $id_kelas_diampu)->get();
+
         foreach ($data_anggota_kelas as $anggota_kelas) {
+            $pembelajaran_list = Pembelajaran::with(['mapel.k13_mapping_mapel'])
+                ->where('kelas_id', $anggota_kelas->kelas_id)
+                ->get()
+                ->sortBy(function ($item) {
+                    return optional($item->mapel->k13_mapping_mapel)->nomor_urut ?? 99;
+                });
 
-            $data_nilai_kelompok_a = K13NilaiAkhirRaport::whereIn('pembelajaran_id', $data_id_pembelajaran_a)->where('anggota_kelas_id', $anggota_kelas->id)->get();
+            $total_nilai = 0;
+            $count_mapel = 0;
 
-            $anggota_kelas->data_nilai_kelompok_a = $data_nilai_kelompok_a;
+            foreach ($pembelajaran_list as $pembelajaran) {
+                $rencana_ids = K13RencanaKisi::where('pembelajaran_id', $pembelajaran->id)->pluck('id');
+                if ($rencana_ids->isEmpty())
+                    continue;
 
-            $rt_pengetahuan = K13NilaiAkhirRaport::whereIn('pembelajaran_id', $data_id_pembelajaran_all)->where('anggota_kelas_id', $anggota_kelas->id)->avg('nilai_pengetahuan');
-            $rt_keterampilan = K13NilaiAkhirRaport::whereIn('pembelajaran_id', $data_id_pembelajaran_all)->where('anggota_kelas_id', $anggota_kelas->id)->avg('nilai_keterampilan');
+                $avg = K13NilaiKisi::whereIn('k13_rencana_kisi_id', $rencana_ids)
+                    ->where('anggota_kelas_id', $anggota_kelas->id)
+                    ->avg('nilai');
 
-            $anggota_kelas->rata_rata_pengetahuan = round($rt_pengetahuan, 0);
-            $anggota_kelas->rata_rata_keterampilan = round($rt_keterampilan, 0);
+                if (is_null($avg))
+                    continue;
 
-            $cek_deskripsi_sikap = K13DeskripsiSikapSiswa::where('anggota_kelas_id', $anggota_kelas->id)->first();
-            if (is_null($cek_deskripsi_sikap)) {
-                $anggota_kelas->nilai_spiritual = '-';
-                $anggota_kelas->nilai_sosial = '-';
-            } else {
-                $anggota_kelas->nilai_spiritual = $cek_deskripsi_sikap->nilai_spiritual;
-                $anggota_kelas->nilai_sosial = $cek_deskripsi_sikap->nilai_sosial;
+                $total_nilai += round($avg, 0);
+                $count_mapel++;
             }
 
-            $anggota_kelas->data_nilai_ekstrakulikuler = Ekstrakulikuler::where('tapel_id', $tapel->id)->get();
-
-            foreach ($anggota_kelas->data_nilai_ekstrakulikuler as $data_nilai_ekstrakulikuler) {
-                $cek_anggota_ekstra = AnggotaEkstrakulikuler::where('ekstrakulikuler_id', $data_nilai_ekstrakulikuler->id)->where('anggota_kelas_id', $anggota_kelas->id)->first();
-                if (is_null($cek_anggota_ekstra)) {
-                    $data_nilai_ekstrakulikuler->nilai = '-';
-                } else {
-                    $cek_nilai_ekstra = NilaiEkstrakulikuler::where('ekstrakulikuler_id', $data_nilai_ekstrakulikuler->id)->where('anggota_ekstrakulikuler_id', $cek_anggota_ekstra->id)->first();
-                    if (is_null($cek_nilai_ekstra)) {
-                        $data_nilai_ekstrakulikuler->nilai = '-';
-                    } else {
-                        $data_nilai_ekstrakulikuler->nilai = $cek_nilai_ekstra->nilai;
-                    }
-                }
-            }
+            $anggota_kelas->rata_rata_semua = $count_mapel > 0 ? round($total_nilai / $count_mapel, 0) : '-';
+            $anggota_kelas->kehadiran_siswa = KehadiranSiswa::where('anggota_kelas_id', $anggota_kelas->id)->first();
         }
 
-        return view('walikelas.k13.leger.index', compact('title', 'data_mapel_kelompok_a', 'data_mapel_kelompok_b', 'data_ekstrakulikuler', 'count_ekstrakulikuler', 'data_anggota_kelas'));
+        return view('walikelas.k13.leger.index', compact('title', 'sekolah', 'data_anggota_kelas'));
     }
 
     public function export()
